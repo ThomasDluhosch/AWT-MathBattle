3bdf35c2bcf8166d3d8ece66c599d8e90bc0bdf0
ha-da-ra <gold85600@gmail.com>
2024-06-20 15:39:35 +0200

    add jwt authentitcation middleware

---
+++ mathBattle/server/src/middleware/authenticate.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from 'jsonwebtoken';
+
+
+export function authenticate(req: Request, res: Response, next: NextFunction) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.SECRET || "SECRET", (err: any, user: any) => {
+        if (err) {
+            console.log(err)
+            return res.sendStatus(403)
+        }
+        next()
+    });
+}
