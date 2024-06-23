065c36e3de6f977db1610c12dde8897e8e7638fb
ha-da-ra <gold85600@gmail.com>
2024-06-23 15:51:55 +0200

    add medals to levelmap & adjust documentation

--- mathBattle/server/src/levels/get-level-map.ts
+++ mathBattle/server/src/levels/get-level-map.ts
@@ -7,6 +7,6 @@ export async function getLevelMap(req: Request, res: Response){
     const user = req.user;
     if(!user) return res.status(403).send("No user set");
     console.log(user.username);
-    const userLevels = await LevelStatisticsModel.find({username: user.username}).select({_id: 0,number: 1, completed: 1}).exec();
+    const userLevels = await LevelStatisticsModel.find({username: user.username}).select({_id: 0,number: 1, completed: 1, medals: 1}).exec();
     res.status(200).send(userLevels);
 }
