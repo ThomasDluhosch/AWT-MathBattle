17aca7d752dca2fa3f5f7ab277c1c8415d7730f2
laurasalop03 <116661256+laurasalop03@users.noreply.github.com>
2024-06-29 20:00:25 +0200

    add go back button to Options

--- mathBattle/client/src/StaticPages/Options.tsx
+++ mathBattle/client/src/StaticPages/Options.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Slider from "@mui/material/Slider";
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
@@ -8,6 +8,8 @@ import Select, { SelectChangeEvent } from "@mui/material/Select";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Switch from "@mui/material/Switch";
+import { NavBar } from "../NavBar";
+import { useNavigate } from "react-router-dom";
 
 const Options: React.FC = () => {
   const [volume, setVolume] = useState<number>(30);
@@ -21,50 +23,62 @@ const Options: React.FC = () => {
     setVolume(newValue as number);
   };
 
+  const navigate = useNavigate();
+  const returnToMap = () => {
+    navigate("/");
+  };
+
   return (
-    <Box
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-        gap: "20px",
-        height: "100vh",
-      }}
-    >
-      <Box width={300}>
-        <Typography gutterBottom>Volume</Typography>
-        <Slider
-          value={volume}
-          onChange={handleVolumeChange}
-          aria-labelledby="volume-slider"
-          valueLabelDisplay="auto"
-          min={0}
-          max={100}
-        />
-      </Box>
+    <div>
+      <NavBar />
 
-      <Box sx={{ minWidth: 300 }}>
-        <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">Font Size</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={font}
-            label="Font Size"
-            onChange={handleChange}
-          >
-            <MenuItem value={10}>Small</MenuItem>
-            <MenuItem value={20}>Medium</MenuItem>
-            <MenuItem value={30}>Large</MenuItem>
-          </Select>
-        </FormControl>
-      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          gap: "20px",
+          height: "100vh",
+        }}
+      >
+        <Box width={300}>
+          <Typography gutterBottom>Volume</Typography>
+          <Slider
+            value={volume}
+            onChange={handleVolumeChange}
+            aria-labelledby="volume-slider"
+            valueLabelDisplay="auto"
+            min={0}
+            max={100}
+          />
+        </Box>
 
-      <FormGroup>
-        <FormControlLabel control={<Switch defaultChecked />} label="Timer" />
-      </FormGroup>
-    </Box>
+        <Box sx={{ minWidth: 300 }}>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Font Size</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={font}
+              label="Font Size"
+              onChange={handleChange}
+            >
+              <MenuItem value={10}>Small</MenuItem>
+              <MenuItem value={20}>Medium</MenuItem>
+              <MenuItem value={30}>Large</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+
+        <FormGroup>
+          <FormControlLabel control={<Switch defaultChecked />} label="Timer" />
+        </FormGroup>
+
+        <Button onClick={() => navigate(-1)} variant="contained">Go back</Button><br />
+
+      </Box>
+    </div>
   );
 };
 
