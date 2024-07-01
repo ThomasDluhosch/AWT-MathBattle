06554ff1df457951ee439f8c3871ecf041ac2deb
ha-da-ra <gold85600@gmail.com>
2024-07-01 15:21:38 +0200

    make hearts dynamic

--- mathBattle/client/src/Level/Level.tsx
+++ mathBattle/client/src/Level/Level.tsx
@@ -11,6 +11,7 @@ export function Level(props: { levelID: number }) {
 
     const getLevelBattle = useLevelBattleService();
     const [levelBattle, setLevelBattle] = useState<ILevelBattle>();
+    const [playerHealth, setPlayerHealth] = useState<number>(3);
 
     useEffect(() => {
         getLevelBattle(props.levelID).then((result) => {
@@ -20,7 +21,7 @@ export function Level(props: { levelID: number }) {
 
     const time = 80;
     const maxHealth = levelBattle?.monsterHealth;   // initial health
-    const health = levelBattle?.monsterHealth; 
+    const health = levelBattle?.monsterHealth;
     const getHealthInPercent = () => {
         return health && maxHealth ? (maxHealth / health) * 100 : 0;
     }
@@ -29,12 +30,14 @@ export function Level(props: { levelID: number }) {
 
         <div>
             <NavBar />
-            <Box textAlign='center' sx={{m:3, mt: 8 }}>
+            <Box textAlign='center' sx={{ m: 3, mt: 8 }}>
                 <Grid container>
                     <Grid item xs={12}>
-                        <img src="/public/heart.svg" style={{ width: '5em' }} />
-                        <img src="/public/heart.svg" style={{ width: '5em' }} />
-                        <img src="/public/heart-lost.svg" style={{ width: '5em' }} />
+                        {
+                            [1, 2, 3].map((v) => v <= playerHealth ?
+                                <img src="/public/heart.svg" style={{ width: '5em' }} /> :
+                                <img src="/public/heart-lost.svg" style={{ width: '5em' }} />)
+                        }
                     </Grid>
                     <Grid item xs={12} lg={6}>
                         <Typography sx={taskStyle} variant="h1">99 + 1</Typography>
@@ -42,7 +45,7 @@ export function Level(props: { levelID: number }) {
                         <TextField
                             sx={taskStyle} fullWidth placeholder="Solution"></TextField>
                         <br />
-                        <LinearProgress color="secondary" variant="determinate" value={time} sx={{...barStyle, mt:2}} />
+                        <LinearProgress color="secondary" variant="determinate" value={time} sx={{ ...barStyle, mt: 2 }} />
                         <br />
                         <Typography textAlign="center" variant="body1">{`${time} seconds`}</Typography>
                     </Grid>
