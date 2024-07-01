33896b81ef5987455bec24dc140bc78f573f21a9
ha-da-ra <gold85600@gmail.com>
2024-07-01 14:15:19 +0200

    add mock levels

--- mathBattle/client/src/LevelMap/useLevelMapService.ts
+++ mathBattle/client/src/LevelMap/useLevelMapService.ts
@@ -9,14 +9,18 @@ export function useLevelMapService() {
     const authContext = useAuthentication();
 
     const getLevels = async () => {
+        if (import.meta.env.VITE_USE_BACKEND != "TRUE") {
+           return mockLevels;
+        }
+
         if (authContext.token == null) return undefined;
         const mapResponse = await fetchFromBackendAuth("/levels", "get", authContext.token);
-        
+
         if (mapResponse.status == 403) {
             authContext.setToken(null);
             navigate("/login");
         } else if (mapResponse.status == 200) {
-            const levels: ILevel[] = await mapResponse.json();
+            const levels = await mapResponse.json();
             return levels;
         } else {
             console.error("Error while loading levels %s", mapResponse.status);
@@ -26,3 +30,43 @@ export function useLevelMapService() {
 
     return getLevels;
 }
+
+const mockLevels: ILevel[] = [
+    {
+        number: 1,
+        completed: false,
+        medals: {
+            addition: false,
+            division: false,
+            multiplication: false,
+            subtraction: false,
+        }
+    },  {
+        number: 2,
+        completed: false,
+        medals: {
+            addition: false,
+            division: false,
+            multiplication: false,
+            subtraction: false,
+        }
+    },{
+        number: 3,
+        completed: false,
+        medals: {
+            addition: false,
+            division: false,
+            multiplication: false,
+            subtraction: false,
+        }
+    },{
+        number: 4,
+        completed: false,
+        medals: {
+            addition: false,
+            division: false,
+            multiplication: false,
+            subtraction: false,
+        }
+    }
+];
