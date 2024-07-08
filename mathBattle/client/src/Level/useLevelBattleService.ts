1826f901776a6084b97ecd77875e61aef2666ab4
ha-da-ra <gold85600@gmail.com>
2024-07-08 13:20:36 +0200

    add searchparam for calc type

--- mathBattle/client/src/Level/useLevelBattleService.ts
+++ mathBattle/client/src/Level/useLevelBattleService.ts
@@ -1,15 +1,19 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { useAuthentication } from "../Authentication/useAuthentication";
 import { fetchFromBackendAuth } from "../fetch/fetch-backend";
 import { ILevelBattle } from "../Interfaces/ILevelBattle";
+import { CalcType } from "../Interfaces/CalcType";
 
 export function useLevelBattleService() : [(id: number | undefined) => Promise<ILevelBattle | undefined>, (id: number | undefined, score: number) => Promise<boolean | undefined>] {
     const navigate = useNavigate();
     const authContext = useAuthentication();
+    const [searchParams, setSearchParams] = useSearchParams();
+    const typeParam = searchParams.get("type");
+    const calcType :CalcType = typeParam ? parseInt(typeParam) : 0;
 
     const getLevelBattle = async (id: number | undefined) => {
         if (authContext.token == null) return undefined;
-        const mapResponse = await fetchFromBackendAuth("/levels/" + id + "/battle", "GET", authContext.token);
+        const mapResponse = await fetchFromBackendAuth("/levels/" + id + "/battle?calcType=" + calcType, "GET", authContext.token);
 
         if (mapResponse.status == 403 || id == undefined) {
             console.error("No level id given");
