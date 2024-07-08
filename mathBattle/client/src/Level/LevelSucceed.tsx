02322c2bb6d848dd9104e1bd7a69785a098e7df3
ha-da-ra <gold85600@gmail.com>
2024-07-08 21:16:46 +0200

    add some styling to success

--- mathBattle/client/src/Level/LevelSucceed.tsx
+++ mathBattle/client/src/Level/LevelSucceed.tsx
@@ -1,4 +1,4 @@
-import { Typography, Box, Button, Grid, ButtonGroup, Icon } from "@mui/material";
+import { Typography, Box, Button, Grid, ButtonGroup, Icon, useTheme } from "@mui/material";
 import { NavBar } from "../NavBar";
 import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 import { useLevelId } from "./useLevelId";
@@ -11,13 +11,13 @@ export function LevelSucceed() {
     const [searchParams, setSearchParams] = useSearchParams();
     const score = searchParams.get("score");
     const time = searchParams.get("time");
-    
-    const [levelHighscores, setLevelHighscores] = useState<[{username: String, score: Number}]>();
+    const theme = useTheme();
+    const [levelHighscores, setLevelHighscores] = useState<[{ username: String, score: Number }]>();
     const getLevelHighscores = useLevelHighscoresService();
 
     useEffect(() => {
         getLevelHighscores(levelId).then((result) => {
-            if(result) setLevelHighscores(result);
+            if (result) setLevelHighscores(result);
         });
     }, []);
 
@@ -40,17 +40,12 @@ export function LevelSucceed() {
                     <Grid item xs={12}>
                         <Typography variant="h1">Well done!</Typography>
                         <Typography variant="h6">
-                            You solved this level in just {time} seconds!
+                            You solved this level in just  <span style={{color: theme.palette.primary.dark}}><b>{time}</b></span> seconds!
                         </Typography>
                         <Typography variant="h6">
-                            Your score is {score}
+                            Your score is <span style={{color: theme.palette.primary.dark}}><b>{score}</b></span>
                         </Typography>
-                        <Typography variant="h4">Highscores</Typography>
-                        {levelHighscores?.map((e) => (
-                            <Typography variant="h6">{e.username}: {e.score.toString()}</Typography>
-                        ))}
                     </Grid>
-
                     <Grid item xs={12}>
                         <Grid container spacing={2} justifyContent="center">
                             <Grid item>
@@ -63,9 +58,9 @@ export function LevelSucceed() {
                                 </Button>
                             </Grid>
                             <Grid item>
-                                <Button variant="contained" 
-                                startIcon={<Icon>skip_next</Icon>}
-                                onClick={goNext}>Next Level</Button>
+                                <Button variant="contained"
+                                    startIcon={<Icon>skip_next</Icon>}
+                                    onClick={goNext}>Next Level</Button>
                             </Grid>
                             <Grid item>
                                 <Button variant="text"
@@ -76,6 +71,14 @@ export function LevelSucceed() {
                             </Grid>
                         </Grid>
                     </Grid>
+                    <Grid item xs={12} sx={{ border: 4, borderColor: theme.palette.primary.light, borderRadius: 5, m: 2, p: 2, ml: "20%", mr: "20%", backgroundColor: "rgba(241, 211, 171, 0.7)" }}>
+                        <Typography variant="h4">Highscores</Typography>
+                        {levelHighscores?.map((e, index) => (
+                            <Typography variant="h6">{index + 1}. {e.username}: {e.score.toString()}</Typography>
+                        ))}
+                    </Grid>
+
+
                 </Grid>
             </Box>
         </div>
