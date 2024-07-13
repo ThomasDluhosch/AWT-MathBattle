dcf8f0d7fcdc4033ddd4c4f1e08b97345cdc7afe
ha-da-ra <gold85600@gmail.com>
2024-07-13 15:58:40 +0200

    authenticate options endpoints

--- mathBattle/server/src/user/UserRouter.ts
+++ mathBattle/server/src/user/UserRouter.ts
@@ -5,11 +5,13 @@ import { Request, Response } from "express";
 import { tryProcess } from "../helper/tryProcess";
 import { getUserOptions } from "./get-user-options";
 import { postUserOptions } from "./post-user-options";
+import { authenticate } from "../middleware/authenticate";
 
 
 export const UserRouter = Router();
 
 UserRouter.post("/login", (req: Request, res: Response) => tryProcess(req, res, loginUser));
 UserRouter.post("/register", (req: Request, res: Response) => tryProcess(req, res, registerUser));
+UserRouter.use("/options", authenticate);
 UserRouter.get("/options", (req: Request, res: Response) => tryProcess(req, res, getUserOptions));
 UserRouter.post("/options", (req: Request, res: Response) => tryProcess(req, res, postUserOptions));
