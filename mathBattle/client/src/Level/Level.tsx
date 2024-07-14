46f76bd9ae4115ac62d6e410a9f8ea98dc8fc98b
ThomasDluhosch <t.dluhosch@gmail.com>
2024-07-14 15:57:15 +0200

    changed colors of level task

--- mathBattle/client/src/Level/Level.tsx
+++ mathBattle/client/src/Level/Level.tsx
@@ -221,7 +221,7 @@ export function Level() {
               <span
                 style={{
                   color:
-                    solutionGiven == SolutionGiven.CORRECT ? "green" : "red",
+                    solutionGiven == SolutionGiven.CORRECT ? "#4648ba" : "#b6244f",
                 }}
               >
                 {solutionGiven == SolutionGiven.NO
