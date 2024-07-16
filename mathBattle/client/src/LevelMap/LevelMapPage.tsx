74572b4b7798083fa240999d06c9d722b9e09b2d
ha-da-ra <gold85600@gmail.com>
2024-07-16 13:35:22 +0200

    fix container size

--- mathBattle/client/src/LevelMap/LevelMapPage.tsx
+++ mathBattle/client/src/LevelMap/LevelMapPage.tsx
@@ -32,7 +32,7 @@ export function LevelMapPage() {
   return (
     <div>
       <NavBar />
-      <Container sx={{textAlign:"center", mt: 10}}>
+      <Container sx={{textAlign:"center", mt: 10}} maxWidth="xl">
         <Grid container spacing={2}>
           <Grid item xs={12}>
             <Typography variant="h2">
